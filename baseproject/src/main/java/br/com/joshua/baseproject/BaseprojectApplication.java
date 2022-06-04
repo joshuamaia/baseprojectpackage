745b8d6a3af2c2d4e2fdb01c9f623d9eae2faa16
Joshua Maia Rodrigues <jrodrigues.ilia@alelo.com.br>
2022-06-04 19:24:46 -0300

    added microservice struct

--- baseproject/src/main/java/br/com/joshua/baseproject/BaseprojectApplication.java
+++ baseproject/src/main/java/br/com/joshua/baseproject/BaseprojectApplication.java
@@ -2,11 +2,13 @@ package br.com.joshua.baseproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@EnableEurekaClient
 @OpenAPIDefinition(info = @Info(title = "Base Project API", version = "1.0", description = "Base Project Information"))
 public class BaseprojectApplication {
 
