aaf4dd2384d897375674e79548bfced3f61c0095
Joshua Maia Rodrigues <jrodrigues.ilia@alelo.com.br>
2022-06-20 10:01:45 -0300

    bug fix

--- baseproject/src/main/java/br/com/joshua/baseproject/config/RabbitConfig.java
+++ baseproject/src/main/java/br/com/joshua/baseproject/config/RabbitConfig.java
@@ -13,73 +13,64 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitConfig {
 
-    @Value("${app-config.rabbit.exchange.baseproject}")
-    private String personTopicExchange;
+	@Value("${app-config.rabbit.exchange.baseproject}")
+	private String personTopicExchange;
 
-    @Value("${app-config.rabbit.routingKey.person-create}")
-    private String personCreateKey;
+	@Value("${app-config.rabbit.routingKey.person-create}")
+	private String personCreateKey;
 
-    @Value("${app-config.rabbit.routingKey.person-update}")
-    private String personUpdateKey;
-    
-    @Value("${app-config.rabbit.routingKey.person-delete}")
-    private String personDeleteKey;
+	@Value("${app-config.rabbit.routingKey.person-update}")
+	private String personUpdateKey;
 
-    @Value("${app-config.rabbit.queue.person-create}")
-    private String personCreateMq;
+	@Value("${app-config.rabbit.routingKey.person-delete}")
+	private String personDeleteKey;
 
-    @Value("${app-config.rabbit.queue.person-update}")
-    private String personUpdateMq;
-    
-    @Value("${app-config.rabbit.queue.person-delete}")
-    private String personDeleteMq;
+	@Value("${app-config.rabbit.queue.person-create}")
+	private String personCreateMq;
 
-    @Bean
-    public TopicExchange personTopicExchange() {
-        return new TopicExchange(personTopicExchange);
-    }
+	@Value("${app-config.rabbit.queue.person-update}")
+	private String personUpdateMq;
 
-    @Bean
-    public Queue personCreateMq() {
-        return new Queue(personCreateMq, true);
-    }
+	@Value("${app-config.rabbit.queue.person-delete}")
+	private String personDeleteMq;
 
-    @Bean
-    public Queue personUpdateMq() {
-        return new Queue(personUpdateMq, true);
-    }
-    
-    @Bean
-    public Queue personDeleteMq() {
-        return new Queue(personDeleteMq, true);
-    }
+	@Bean
+	public TopicExchange personTopicExchange() {
+		return new TopicExchange(personTopicExchange);
+	}
 
-    @Bean
-    public Binding personCreatekMqBinding(TopicExchange topicExchange) {
-        return BindingBuilder
-            .bind(personCreateMq())
-            .to(topicExchange)
-            .with(personCreateKey);
-    }
+	@Bean
+	public Queue personCreateMq() {
+		return new Queue(personCreateMq, true);
+	}
 
-    @Bean
-    public Binding personUpdateMqBinding(TopicExchange topicExchange) {
-        return BindingBuilder
-            .bind(personUpdateMq())
-            .to(topicExchange)
-            .with(personUpdateKey);
-    }
-    
-    @Bean
-    public Binding personDeleteMqBinding(TopicExchange topicExchange) {
-        return BindingBuilder
-            .bind(personDeleteMq())
-            .to(topicExchange)
-            .with(personDeleteKey);
-    }
+	@Bean
+	public Queue personUpdateMq() {
+		return new Queue(personUpdateMq, true);
+	}
 
-    @Bean
-    public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
+	@Bean
+	public Queue personDeleteMq() {
+		return new Queue(personDeleteMq, true);
+	}
+
+	@Bean
+	public Binding personCreatekMqBinding(TopicExchange topicExchange) {
+		return BindingBuilder.bind(personCreateMq()).to(topicExchange).with(personCreateKey);
+	}
+
+	@Bean
+	public Binding personUpdateMqBinding(TopicExchange topicExchange) {
+		return BindingBuilder.bind(personUpdateMq()).to(topicExchange).with(personUpdateKey);
+	}
+
+	@Bean
+	public Binding personDeleteMqBinding(TopicExchange topicExchange) {
+		return BindingBuilder.bind(personDeleteMq()).to(topicExchange).with(personDeleteKey);
+	}
+
+	@Bean
+	public MessageConverter jsonMessageConverter() {
+		return new Jackson2JsonMessageConverter();
+	}
 }
--- baseprojectexpense/src/main/java/br/com/joshua/baseprojectexpense/BaseprojectApplication.java
+++ baseprojectexpense/src/main/java/br/com/joshua/baseprojectexpense/BaseprojectApplication.java
@@ -1,5 +1,6 @@
 package br.com.joshua.baseprojectexpense;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -7,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
+@EnableRabbit
 @SpringBootApplication
 @EnableEurekaClient
 @OpenAPIDefinition(info = @Info(title = "Base Project API", version = "1.0", description = "Base Project Information"))
